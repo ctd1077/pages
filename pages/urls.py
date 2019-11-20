5623f0b51f5ed78d0afd2fbfac4a505ec6fab357
Cam Deatherage <ctd1077@gmail.com>
2019-11-19 21:15:52 -0700

    first commit

---
+++ pages/urls.py
@@ -0,0 +1,8 @@
+# pages/urls.py
+
+from django.conf.urls import url
+from . import views
+urlpatterns = [
+url('about/', views.AboutPageView.as_view(), name='about'),   
+url('', views.HomePageView.as_view(), name='home'),
+]
